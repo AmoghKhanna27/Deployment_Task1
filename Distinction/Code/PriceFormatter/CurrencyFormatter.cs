7210eadcb8a1e046b64292cf91f6840376cd4816
AmoghKhanna27 <155412092+AmoghKhanna27@users.noreply.github.com>
2026-08-17 11:08:27 +1000

    Initial commit

---
+++ Distinction/Code/PriceFormatter/CurrencyFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceFormatter
+{
+    public class CurrencyFormatter
+    {
+            public string FormatPrice(decimal price)
+            {
+                return "$" + price.ToString("0.00");
+            }
+        
+    }
+}
---
+++ Distinction/Code/PricingLogic/PricingClassifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingLogic
+{
+    public class PricingClassifier
+    {
+        public string Categorize(decimal price)
+        {
+            if (price < 0)
+            {
+                return "Invalid Price";
+            }
+            else if (price <= 50)
+            {
+                return "Cheap";
+            }
+            else
+            {
+                return "Expensive";
+            }
+        }
+    }
+}
